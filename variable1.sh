6ffac27e10bfd7b1950ef906f9c0d729e1cd5796
Chandu154 <devarachandra4@gmail.com>
2025-05-18 18:19:06 +0530

    Practice shell

--- variable1.sh
+++ variable1.sh
@@ -1,8 +1,10 @@
-NAME1=$(1)
-NAME2=$(2)
+#!bin/bash
+
+NAME1=chandu
+NAME2=damu
 echo $NAME1 ':Hi' $NAME2 'how are you'
 echo $NAME2 ':Hi' $NAME1 'I am doing good , what about you'  
 
 DATE=$(date)
-echo 'This script run at' $DATE 
+echo " This script run at $DATE " 
 
