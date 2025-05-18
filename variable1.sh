4536de4f6875d871e631236db9cae94c6ec928e7
Chandu154 <devarachandra4@gmail.com>
2025-05-18 18:14:14 +0530

    Practice shell

--- variable1.sh
+++ variable1.sh
@@ -1,11 +1,5 @@
-NAME1=$1
-NAME2=$2
-echo $NAME1 ':Hi' $NAME2 'how are you'
-echo $NAME2 ':Hi' $NAME1 'I am doing good , what about you'  
-
-
-NAME1=$1
-NAME2=$2
+NAME1=$(1)
+NAME2=$(2)
 echo $NAME1 ':Hi' $NAME2 'how are you'
 echo $NAME2 ':Hi' $NAME1 'I am doing good , what about you'  
 
