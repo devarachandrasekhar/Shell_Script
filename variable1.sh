ebf627cdeadefb8c78c090f77731b031ea4c27f3
Chandu154 <devarachandra4@gmail.com>
2025-05-18 18:37:41 +0530

    Practice shell

--- variable1.sh
+++ variable1.sh
@@ -27,14 +27,6 @@ echo " This script run at $DATE "
 
 
 
-number1=$1
-
-number1=$2
-
-sum=$((number1+number2))
-
-echo "sum of $number1 and $number1 = $sum" 
-
 
 
 
---
+++ variable2.sh
@@ -0,0 +1,7 @@
+number1=$1
+
+number1=$2
+
+sum=$((number1+number2))
+
+echo "sum of $number1 and $number1 = $sum" 
