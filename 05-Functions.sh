8a32693e084bcf999d1db462f992a71e57d7805c
Chandu154 <devarachandra4@gmail.com>
2025-05-20 07:47:20 +0530

    shell script

--- 05-Functions.sh
+++ 05-Functions.sh
@@ -5,23 +5,22 @@ DATE=$(date)
 
 
 echo "$DATE"
-LOGFILE=/tmp/$DATE.log
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 validate ()
 {
  if [ $1 -ne 0 ]   
    then 
-    echo "$2 is failed"
+    echo -e "$2 is $R failed $N"
    else
-    echo "$2 is succssful"
+    echo "$2 is $G succssful $N"
 fi    
 }
-
-
-
-
-
 
 
 user=$(id -u)
@@ -35,6 +34,7 @@ fi
 
 #installation of my sql, by default in shell script it cannt validate the he previoues commad is successfull or not we need to validate
 
+#installing mysql ad storing logs in tmp folder and validating successfull or not using functions
 
 yum install mysql -y &>>$LOGFILE
 
@@ -45,3 +45,7 @@ validate $? mysql
 yum install postfix -y &>>$LOGFILE
 
 validate $? postfix 
+
+yum install ggit -y &>>$LOGFILE
+
+validate $? git
