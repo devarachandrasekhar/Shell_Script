033a5dae20121b1e2ec7df285540208aa2b9f824
Chandu154 <devarachandra4@gmail.com>
2025-05-20 06:39:12 +0530

    shell script

--- 03-condtions.sh
+++ 03-condtions.sh
@@ -2,15 +2,9 @@
 
 number1=$1
 
-if  [ $number1 -greater 0 ]
-
-then 
-
- echo "The given number $number1 is greater then 0"
-
+if  [ $number1 -gt 0 ]
+ then 
+  echo "The given number $number1 is greater then 0"
  else
-
- echo "echo "The given number $number1 is less then 0""
-
-
+  echo "echo "The given number $number1 is less then 0""
 fi
