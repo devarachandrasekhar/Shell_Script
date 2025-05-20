4c7c716e95b57c0edb714f6b58f24d973341d4d0
Chandu154 <devarachandra4@gmail.com>
2025-05-20 06:35:17 +0530

    shell script

---
+++ 03-condtions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+number1=$1
+
+if  { $number1 -ge 0 
+ then 
+
+ echo "The given number $number1 is greater then 0"
+
+ else
+
+ echo "echo "The given number $number1 is less then 0""
+
+
+fi
+}
