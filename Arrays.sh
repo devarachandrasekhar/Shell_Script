b96269256fab99c6d59c45f434d6e8ee132d41ef
Chandu154 <devarachandra4@gmail.com>
2025-05-20 06:27:44 +0530

    shell script

--- Arrays.sh
+++ Arrays.sh
@@ -4,6 +4,6 @@ PERSONS=("ram","jam","dam")
 
  echo "first person :${PERSONS[1]}" 
 
- echo "first person :${PERSONS[$1]}" 
+ echo "first person :${PERSONS[$0]}" 
 
  echo "first person :${PERSONS[@]}" 
