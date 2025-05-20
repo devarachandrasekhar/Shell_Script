b150f65f7339a89c6aa653a6faf740b4f54a69c2
Chandu154 <devarachandra4@gmail.com>
2025-05-20 08:02:11 +0530

    shell script

--- 07-loops.sh
+++ 07-loops.sh
@@ -1,12 +1,23 @@
 
 
-for i in {1..100}
+for i in {1..10}
 do
 echo $i
 done
 
 
+
+
+
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+
+
+
+
+
 for i in $@
  do
-   yum install $i -y
+   yum install $i -y &>>LOGFILE
  done  
