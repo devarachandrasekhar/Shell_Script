ddb13eccc55bb044b453cc0bbec2e605b05ae6b2
Chandu154 <devarachandra4@gmail.com>
2025-05-20 10:54:44 +0530

    shell script

--- 07-loops.sh
+++ 07-loops.sh
@@ -7,26 +7,3 @@ done
 
 
 
-
-DATE=$(date +%f)
-SCRIPT_NAME=$0
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
-
-
-for i in $@
- do
-   yum install $i -y #&>>LOGFILE
-
-   validate $? $i
-
-   validate ()
-{
-if [ $1 -ne 0 ]
- then 
-  echo "$i failed"
- else
-  echo "$i installed successfully" 
-fi  
-}
- done  
-
---
+++ 08-loops.sh
@@ -0,0 +1,49 @@
+
+DATE=$(date +%f)
+SCRIPT_NAME=$0
+LOGDIR=/home/ec2-user/Shell_Script/shell_logs 
+LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"   
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+    then
+        echo "run with root access"
+    else 
+        echo " $R you have root access $N "  
+
+
+
+for i in $@
+ do
+
+   yum list installed $i
+
+   if [ $? ne -0 ]
+        then
+            echo "$i is not installed lets install it"
+            yum install $i -y &>>LOGFILE
+            validate $? $i
+        else
+            echo "$i alredy installed"
+    fi
+   
+ done  
+
+
+
+validate ()
+{
+if [ $1 -ne 0 ]
+ then 
+  echo -e "$2 $R failed $N "
+ else
+  echo "$2 $G installed successfully $N " 
+fi  
+}
