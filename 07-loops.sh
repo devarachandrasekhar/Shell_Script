38dd00dde40879202f7f1ff3fe280224106dd40b
Chandu154 <devarachandra4@gmail.com>
2025-05-20 09:24:08 +0530

    shell script

--- 07-loops.sh
+++ 07-loops.sh
@@ -16,4 +16,15 @@ LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 for i in $@
  do
    yum install $i -y &>>LOGFILE
+
+   validate $? $i
  done  
+
+validate ()
+{
+if [ $? -ne 0 ]
+ then 
+  echo "$i failed"
+ else
+  echo "$i installed successfully" 
+}
