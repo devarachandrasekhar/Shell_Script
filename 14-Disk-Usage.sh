fd74b6349308738081bf78b0d1ffa286d2a0a538
Chandu154 <devarachandra4@gmail.com>
2025-05-22 09:24:26 +0530

    shell script

--- 14-Disk-Usage.sh
+++ 14-Disk-Usage.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
-#clours
-#validations
-# log redirections                
+# colors
+# validations
+# log redirections
 
-DATE=$(date +%F)
-
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
-LOGSDIR=/tmp
-#/home/centos/shellscript-logs-scriptname-date.log
-LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
 
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-Y="\e[33m"    
+Y="\e[33m"
 
 DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
 message=""
 
-# IFS = Internal field  seperator
-
+#IFS= means internal feild seperator is space.
 while IFS= read line
-do 
- # this command will give you usage in number formate for comparison
-usage=$(echo  $line | awk '{print$6}'|cut -d % -f1)
- #This commmand will give us parttion
-partition=$(echo $line | awk '{print$1}') 
-# now we need to check more then threshold or not 
- if [ $usage -gt $DISK_USAGE_THRESHOLD ]
-  then
-  message+="HIGH DISK USAGE on $partition: $usage\n"
-  fi 
+do  
+    # this command will give you usage in number format for comparision
+    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+    # this command will give us partition
+    partition=$(echo $line | awk '{print $1}')
+    #now you need to check whether it is more than threshold or not
+    if [ $usage -gt $DISK_USAGE_THRESHOLD ];
+    then
+        message+="HIGH DISK USAGE on $partition: $usage\n"
+    fi
 done <<< $DISK_USAGE
 
 echo -e "message: $message"
 
-#echo "$message" | mail -s "High Disk Usage" devarachandra4@gmail.com
+#echo "$message" | mail -s "High Disk usage" info@joindevops.com
 
-sh mail.sh devarachandra4@gmail.com "High Disk Usage" "$message" "Devopsteam" "This is the alert"
+#how to call other shell script from your current script
+sh mail.sh devarachandra4@gmail.com "High Disk Usage" "$message" "DEVOPS TEAM" "High Disk usage"
