8c970822a649d7331d8ecfc2f1a301206774a4e5
Chandu154 <devarachandra4@gmail.com>
2025-06-03 07:02:14 +0530

    learning ansible

--- practice.sh
+++ practice.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-var1=chandu
-var2=ram
+# var1=chandu
+# var2=ram
 
-echo "$var1 and $var2 are good friends"
+# echo "$var1 and $var2 are good friends"
+
+var1=$1
+var2=$2 
+
+echo "$var1 and $var2 are good friens"
