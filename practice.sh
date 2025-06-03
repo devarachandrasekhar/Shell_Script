bc5a91c9352890f1e9fc86241f8c7b9553c2e875
Chandu154 <devarachandra4@gmail.com>
2025-06-03 07:11:09 +0530

    learning ansible

--- practice.sh
+++ practice.sh
@@ -9,6 +9,16 @@ var1=$1
 var2=$2 
 DATE=$(date)
 
+
+validate () {
+
+if [ $? -ne 0 ]
+ then 
+ echo "last command failed"
+ exit 1
+fi 
+}
+
 echo "$var1 and $var2 are good friend till $DATE"
 
 echo "$?"
@@ -20,11 +30,3 @@ echo "$#"
 echo "$0"
 
 
-validate () {
-
-if [ $? -ne 0 ]
- then 
- echo "last command failed"
- exit 1
-fi 
-}
