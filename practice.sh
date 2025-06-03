3f43d13bdba73d793824493894914cc117110602
Chandu154 <devarachandra4@gmail.com>
2025-06-03 07:39:15 +0530

    learning ansible

--- practice.sh
+++ practice.sh
@@ -51,16 +51,12 @@
 
 names=("ram","jam","dam","shyam")
 
-echo "person1 ${names[0]}"
+echo "person1 ${names[$0]}"
 
-echo "person2 ${names[1]}"
+echo "person2 ${names[$1]}"
 
 
-PERSONS=("ram","jam","dam")
 
- echo "first person :${PERSONS[0]}" 
-
- echo "first person :${PERSONS[$1]}" 
 
 
 
