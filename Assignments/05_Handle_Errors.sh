c9cfe2ab6869f7f79ba4213130af032d7223e864
Chandu154 <devarachandra4@gmail.com>
2025-07-18 06:20:05 +0530

    learning

--- Assignments/05_Handle_Errors.sh
+++ Assignments/05_Handle_Errors.sh
@@ -5,9 +5,6 @@ trap 'echo "x error on line $LINENO: $BASH_COMMAND": exit 1' ERR
 
 echo "step 1: listing directory"
 
-ls /home/ec2-user/  #this will trigger the trap 
+ls /home/ddd/  #this will trigger the trap 
 
 echo " Step 2 :This wont execeute due to above error"
-
-
-ls /home/ec2-usefffr/  #this will trigger the trap 
